554c6162d2b5243497f7eb80259b93d3b1b63cd0
qwertyyb <qwertyyb@foxmail.com>
2018-01-11 20:12:23 +0800

    添加自动上传xml和crx到七牛云；添加自动打包crx

--- build/publish.js
+++ build/publish.js
@@ -1,22 +1,17 @@
 var qiniu = require("qiniu");
 
-
-var key='test.mp4';
-
-
-
 //需要填写你的 Access Key 和 Secret Key
 var accessKey = 'ee6pZ2JdJFo_ghWAbpX1CpX3nXUSDvXagOINEqjb',
     secretKey = 'J415z4sBMq6tnlCaTm9UKpl_onGt6pG3UiTls1u6',
     //要上传的空间
-    bucket = 'cdns',
+    bucket = 'cdns'
     //上传到七牛后保存的文件名
-    key = 'adblock/videoAdBlock.crx',
+    // key = 'adblock/videoAdBlock.crx',
     //要上传文件的本地路径
-    localFile = "videoAdBlock.crx";
+    // localFile = "videoAdBlock.crx";
 
 //构建上传策略函数
-function getUpToken(bucket, key) {
+function getUpToken(key) {
     var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
     var putPolicy = new qiniu.rs.PutPolicy({
         scope: bucket+":"+key,
@@ -46,7 +41,7 @@ function uploadFile(localFile, key, token) {
         }
     
         if (respInfo.statusCode == 200) {
-            console.log("上传成功:" + respBody);
+            console.log(localFile + " 上传成功:", respBody);
         } else {
             console.log(respInfo.statusCode);
             console.log(respBody);
@@ -55,5 +50,10 @@ function uploadFile(localFile, key, token) {
 }
 
 //生成上传 Token
-token = getUpToken(bucket, key);
-uploadFile(localFile, key, token);
+// token = getUpToken(key);
+// uploadFile(localFile, key, token);
+
+module.exports = function(localPath, key) {
+    var token = getUpToken(key)
+    uploadFile(localPath, key, token)
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,80 @@
 var gulp = require('gulp')
 var uglify = require('gulp-uglify')
-var pump = require('pump')
-var copy = require('copy')
 var del = require('del')
+var jsonfile = require('jsonfile')
+var fs = require('fs')
+var crx = require('gulp-crx-pack')
+var uploadFile = require('./build/publish')
+console.log(uploadFile)
+var info = {}
 
-gulp.task('minifyJs', cb => {
-    pump([
-        gulp.src('src/scripts/**/*.js'),
-        uglify(),
-        gulp.dest('dist/scripts/')
-    ], cb)
+// 压缩 src/scripts/ 目录下的js文件
+gulp.task('minifyJs', done => {
+    return gulp.src('src/scripts/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts/'))
+    done()
 })
-gulp.task('copy', cb => {
-    copy(['src/*.html', 'src/*.json', 'src/icons/*.png'], 'dist/', cb)
+
+// 把除src/中除src/scripts/全部copy到dist/目录
+gulp.task('copy', done => {
+    gulp.src(['src/*.html', 'src/*.json', 'src/**/*.png'])
+    .pipe(gulp.dest('dist/'))
+    done()
 })
-gulp.task('clean', cb => {
+// 清除dist/的文件
+gulp.task('clean', done => {
     del.sync('dist/*')
-    cb()
+    done()
 })
-gulp.task('build', ['clean', 'copy', 'minifyJs'], cb => cb())
+
+// 自动更新manifest.json中的版本
+gulp.task('updateVersion', done => {
+    var file = 'src/manifest.json'
+    var obj = jsonfile.readFileSync(file)
+    // 更新manifest.json中的相关字段信息
+    var curVersion = Number(obj.version)
+    obj.version = (curVersion + 0.1).toFixed(2).toString()
+    // 更新update_url字段，更新版本号
+    var updateUrl = obj.update_url.split('?')[0] + '?v' + obj.version
+    obj.update_url = updateUrl
+
+    // 获取相关信息：
+    info = {
+        name: obj.name,
+        version: obj.version,
+        updateUrl: obj.update_url
+    }
+    // 写入文件
+    jsonfile.writeFileSync(file, obj, {spaces: 4})
+    done()
+})
+
+gulp.task('package', done => {
+    var codebase = 'http://7xv7aw.com1.z0.glb.clouddn.com/adblock/videoAdBlock.crx?v' + info.version
+    info.crx = codebase
+    gulp.src('./dist/')
+    .pipe(crx({
+        privateKey: fs.readFileSync('./videoAdBlock.pem', 'utf8'),
+        filename: 'videoAdBlock.crx',
+        codebase: codebase,
+        updateXmlFilename: 'version.xml'
+    }))
+    .pipe(gulp.dest('.'))
+    done()
+    
+})
+// 上传version.xml和videoAdBlock.crx到七牛云
+gulp.task('upload', done => {
+    // 上传version.xml
+    uploadFile('./version.xml', 'adblock/version.xml')
+    uploadFile('./videoAdBlock.crx', 'adblock/videoAdBlock.crx')
+    done()
+})
+gulp.task('build', gulp.series(
+    gulp.parallel('updateVersion', 'clean'), 
+    gulp.parallel('copy', 'minifyJs'),
+    'package', 'upload', done => {
+        console.log('打包完成\n', info)
+        done()
+    }))
